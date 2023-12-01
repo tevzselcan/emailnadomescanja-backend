273a7132be5d72e16d086af14d857f2094527ac9
tevzselcan <tevz.selcan@gmail.com>
2023-12-01 15:11:42 +0100

    Update cron schedule for sending substitutions email

--- src/modules/cron/cron.service.ts
+++ src/modules/cron/cron.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import Logging from 'library/Logging';
-import { MailService } from 'modules/mail/mail.service';
 import { SubstitutionsService } from 'modules/substitutions/substitutions.service';
-import { UsersService } from 'modules/users/users.service';
 
 @Injectable()
 export class CronService {
   constructor(private substitsionService: SubstitutionsService) {}
-  @Cron('45 * * * * *')
+  @Cron('0 19 * * 0-4')
   async sendSubtitutionsMail() {
     try {
       await this.substitsionService.setSubstitutions();
